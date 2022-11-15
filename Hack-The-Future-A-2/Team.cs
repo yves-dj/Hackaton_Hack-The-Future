581a41c5760c1dfa4733302d6bd5febd7a2228bf
Arne Janssens <arne.janssens@student.ap.be>
2022-11-15 15:39:11 +0100

    small fix loss of code

---
+++ Hack-The-Future-A-2/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack_The_Future_A_2
+{
+    public class Team
+    {
+        public List<Wizard> TeamA { get; set; }
+        public List<Wizard> TeamB { get; set; }
+    }
+}
---
+++ Hack-The-Future-A-2/Wizard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack_The_Future_A_2
+{
+    public class Wizard
+    {
+        public int Strength { get; set; }
+        public int Speed { get; set; }
+        public int Health { get; set; }
+    }
+}
