526209fa5d0536c4b59e16527e71b4c0808efdc8
Arne Janssens <arne.janssens@student.ap.be>
2022-11-15 11:29:48 +0100

    Challenge 1-A complete

---
+++ Hack-The-Future-A-1/NumeralConvertor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hackaton_Hack_The_Future
+{
+    public static class NumeralConvertor
+    {
+        public static int RomanToInt(string s)
+        {
+            int sum = 0;
+            Dictionary<char, int> romanNumbersDictionary = new()
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 }
+            };
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char currentRomanChar = s[i];
+                romanNumbersDictionary.TryGetValue(currentRomanChar, out int num);
+                if (i + 1 < s.Length && romanNumbersDictionary[s[i + 1]] > romanNumbersDictionary[currentRomanChar])
+                {
+                    sum -= num;
+                }
+                else
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+
+        public static string IntToRoman(int num)
+        {
+            string romanResult = string.Empty;
+            string[] romanLetters = {
+                "M",
+                "CM",
+                "D",
+                "CD",
+                "C",
+                "XC",
+                "L",
+                "XL",
+                "X",
+                "IX",
+                "V",
+                "IV",
+                "I"
+            };
+            int[] numbers = {
+                1000,
+                900,
+                500,
+                400,
+                100,
+                90,
+                50,
+                40,
+                10,
+                9,
+                5,
+                4,
+                1
+            };
+
+            int i = 0;
+            while (num != 0)
+            {
+                if (num >= numbers[i])
+                {
+                    num -= numbers[i];
+                    romanResult += romanLetters[i];
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return romanResult;
+        }
+    }
+}
--- Hack-The-Future-A-1/Program.cs
+++ Hack-The-Future-A-1/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Hackaton_Hack_The_Future;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+// Swagger
+// https://app-htf-2022.azurewebsites.net/swagger/index.html
+// De httpclient die we gebruiken om http calls te maken
+var client = new HttpClient();
+
+// De base url die voor alle calls hetzelfde is
+client.BaseAddress = new Uri("https://app-htf-2022.azurewebsites.net");
+
+// De token die je gebruikt om je team te authenticeren, deze kan je via de swagger ophalen met je teamname + password
+var token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiNCIsIm5iZiI6MTY2ODUwMzYyMywiZXhwIjoxNjY4NTkwMDIzLCJpYXQiOjE2Njg1MDM2MjN9.2f_aCgTzO32kWid1TFViwP-HySL3L5W6lh-b--Jod8IwEYuXHRO6vU6OkoPyruCCVywuBLPHSNtFDyPYLuSefg";
+
+// We stellen de token in zodat die wordt meegestuurd bij alle calls, anders krijgen we een 401 Unauthorized response op onze calls
+client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+// De url om de challenge te starten
+var startUrl = "api/path/1/easy/Start";
+
+// We voeren de call uit en wachten op de response
+// https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/operators/await
+// De start endpoint moet je 1 keer oproepen voordat je aan een challenge kan beginnen
+// Krijg je een 403 Forbidden response op je Sample of Puzzle calls? Dat betekent dat de challenge niet gestart is
+var startResponse = await client.GetAsync(startUrl);
+
+// De url om de sample challenge data op te halen
+var sampleUrl = "api/path/1/easy/Sample";
+
+// We doen de GET request en wachten op de het antwoord
+// De response die we verwachten is een lijst van getallen dus gebruiken we List<int>
+var sampleGetResponse = await client.GetFromJsonAsync<List<string>>(sampleUrl);
+
+string romanSum = GetAnswer(sampleGetResponse);
+
+// We sturen het antwoord met een POST request
+// Het antwoord dat we moeten versturen is een getal dus gebruiken we int
+// De response die we krijgen zal ons zeggen of ons antwoord juist was
+var samplePostResponse = await client.PostAsJsonAsync<string>(sampleUrl, romanSum);
+
+// Om te zien of ons antwoord juist was moeten we de response uitlezen
+// Een 200 status code betekent dus niet dat je antwoord juist was!
+var samplePostResponseValue = await samplePostResponse.Content.ReadAsStringAsync();
+
+Console.WriteLine(samplePostResponseValue);
+
+// De url om de puzzle challenge data op te halen
+var puzzleUrl = "api/path/1/easy/Puzzle";
+// We doen de GET request en wachten op de het antwoord
+// De response die we verwachten is een lijst van getallen dus gebruiken we List<int>
+var puzzleGetResponse = await client.GetFromJsonAsync<List<string>>(puzzleUrl);
+
+// Je zoekt het antwoord
+var puzzleAnswer = GetAnswer(puzzleGetResponse);
+
+// We sturen het antwoord met een POST request
+// Het antwoord dat we moeten versturen is een getal dus gebruiken we int
+// De response die we krijgen zal ons zeggen of ons antwoord juist was
+var puzzlePostResponse = await client.PostAsJsonAsync<string>(puzzleUrl, puzzleAnswer);
+
+// Om te zien of ons antwoord juist was moeten we de response uitlezen
+// Een 200 status code betekent dus niet dat je antwoord juist was!
+var puzzlePostResponseValue = await puzzlePostResponse.Content.ReadAsStringAsync();
+Console.WriteLine(puzzlePostResponseValue);
+Console.ReadLine();
+
+static string GetAnswer(List<string>? getResponse)
+{
+    List<int> convertedRomans = new List<int>();
+
+    foreach (string item in getResponse)
+    {
+        int convertedNumeral = NumeralConvertor.RomanToInt(item);
+        convertedRomans.Add(convertedNumeral);
+    }
+
+    int sumNumerals = convertedRomans.Sum();
+    string romanSum = NumeralConvertor.IntToRoman(sumNumerals);
+    Console.WriteLine(romanSum);
+    return romanSum;
+}
